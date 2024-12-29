510807c9d05f49a48ece954a231ccafd0270d990
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-12-29 12:38:28 +0530

    Main UI changed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2811962774"
+    serviceWorkerVersion: "907219195"
   }
 });
